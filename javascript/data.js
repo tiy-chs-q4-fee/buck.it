07881804e9f31d56dfcf161b8f7422ebd44d1c9a
Cory Figueroa <c.t.figueroa@gmail.com>
2014-08-13 14:43:46 -0400

    added bucket list data to data.js

--- javascript/data.js
+++ javascript/data.js
@@ -1,1 +1,39 @@
+var travelList = [
 
+  "Visit 5 countries",
+  "See all 50 states",
+  "See the 7 wonders of the world"
+
+];
+
+var sportsList = [
+
+  "Go to the Olympics",
+  "Go to the Super Bowl",
+  "Watch a Finals Game 7"
+
+];
+
+var exerciseList = [
+
+  "Run a marathon",
+  "Do a triathlon",
+  "Join a Crossfit group"
+
+];
+
+var daredevilList = [
+
+  "Go skydiving",
+  "Go on a safari",
+  "Go swimming with sharks"
+
+];
+
+var learningList = [
+
+  "Get a degree",
+  "Learn a language",
+  "Code a website"
+
+]
