e91407d5f87dc5a44e10637ebedcdd2f560d618f
Cory Figueroa <c.t.figueroa@gmail.com>
2014-08-18 11:40:04 -0400

    added data for retiree variable in data.js

--- javascript/data.js
+++ javascript/data.js
@@ -1,4 +1,4 @@
-var 1to20 = [
+var teen = [
 {
   travelList: {
     northAmerica:
@@ -39,18 +39,10 @@ var 1to20 = [
     africa:
     asia:
     australia:
-  },
-  luckyList: {
-    northAmerica:
-    southAmerica:
-    europe:
-    africa:
-    asia:
-    australia:
-  },
+  }
 }
 ];
-var 20to50 = [{
+var adult = [{
   travelList: {
     northAmerica:
     southAmerica:
@@ -90,66 +82,50 @@ var 20to50 = [{
     africa:
     asia:
     australia:
-  },
-  luckyList: {
-    northAmerica:
-    southAmerica:
-    europe:
-    africa:
-    asia:
-    australia:
-  },
+  }
 
 }];
-var 50plus = [{
+var retiree = [{
   travelList: {
-    northAmerica:
-    southAmerica:
-    europe:
-    africa:
-    asia:
-    australia:
+    northAmerica:["Ride an Airstream around the 50 states", "Go to Mt. Rushmore", "Go to the Grand Canyon"],
+    southAmerica: ["Take a donkey to Machu Pichu", "Drink caparinhas in Brazil", "Go to the Galapagos Islands"],
+    europe: ["Go to Neuschwanstein in Germany", "Take a Viking River Boat Cruise", "Tour the Palace of Versailles"],
+    africa: ["Ride a camel in the Sahara", "Go glamping in South Africa", "Go to a gorilla sanctuary"],
+    asia: ["Visit the Great Wall of China", "Eat the cuisine of the Szechuan Province", "Learn how to make sake"],
+    australia: ["Camp with the Aborigines", "Go to the Sydney Opera House", "Go sharkwatching on the Great Barrier Reef"]
   },
   exerciseList: {
-    northAmerica:
-    southAmerica:
-    europe:
-    africa:
-    asia:
-    australia:
+    northAmerica: ["Run the NYC marathon", "Swim to Alcatraz", "Compete in the Ironman Championships"],
+    southAmerica: ["Run a marathon", "Hike to Machu Pichu", "Go skiing in Argentina"],
+    europe: ["Run a marathon", "Compete in the wife carrying race in Finland", "Paddleboard Amsterdam"],
+    africa: ["Run with the Masai Warriors", "Compete in Ironman Africa", "Climb Mt. Kilamanjaro"],
+    asia: ["Run a marathon", "Bike part of the Great Wall", "Hike Mt. Fuji"],
+    australia: ["Run a marathon", "Compete in the Ironman Australia", "Hike Ayers Rock"]
   },
   sportsList: {
-    northAmerica:
-    southAmerica:
-    europe:
-    africa:
-    asia:
-    australia:
+    northAmerica: ["Go to the Super Bowl", "Attend March Madness", "Visit Fenway Park"],
+    southAmerica: ["Watch a soccer game in the Maracana", "Play pato with gauchos", "Paddle the Amazon"],
+    europe: ["Attend a bullfight in Spain", "Go to Camp Nou & watch FC Barcelona", "Play golf at St. Andrews"],
+    africa: ["Watch a rugby match", "Watch the Dakar Rally off-road race", "Play golf at Fancourt Links"],
+    asia: ["Watch a cricket match in India", "Attend a horse race in Shanghai", "Play a round of golf in China"],
+    australia: ["Watch the NZ All Blacks play a rugby game", "Try surfing at Bondi Beach", "Play golf at Royal Melbourne"]
   },
   daredevilList: {
-    northAmerica:
-    southAmerica:
-    europe:
-    africa:
-    asia:
-    australia:
+    northAmerica: ["Eat Rocky Mountain Oysters", "Hanglide in Whistler, Canada", "Go hot air ballooning in Napa"],
+    southAmerica: ["Swim in the Amazon", "Drink the water", "Take a hot air balloon ride in Chile"],
+    europe: ["Hanglide in Switzerland", "Run with the Bulls", "Compete in the Gloucestershire Cheese Rolling"],
+    africa: ["Go on a safari", "Take a hot air balloon over the Serengeti", "Hike the Sahara"],
+    asia: ["Eat balut", "Take a hot air balloon ride over the Great Wall", "Climb Mt. Everest"],
+    australia: ["Swim at the Great Barrier Reef", "Go sharkfishing", "Skydive"]
   },
   learningList: {
-    northAmerica:
-    southAmerica:
-    europe:
-    africa:
-    asia:
-    australia:
-  },
-  luckyList: {
-    northAmerica: 
-    southAmerica:
-    europe:
-    africa:
-    asia:
-    australia:
-  },
+    northAmerica: ["Visit the Liberty Bell", "Go to Plymouth Rock", "Attend an opera"],
+    southAmerica: ["Attend Carnival", "Watch a Dia de los Muertos parade", "Visit the churches of Chiloe in Chile"],
+    europe: ["Go to the Louvre in Paris", "Visit Stonehenge", "Watch the Russian Ballet"],
+    africa: ["Go to Robben Island", "Go to a Moroccan spice market", "Visit the fossil sites of S. Africa"],
+    asia: ["Visit the Gardens of Suzhou", "View the Terracota Army", "Attend a Tea Ceremony in Japan"],
+    australia: ["View the Uluru", "Go to the Sydney Opera House", "Go to Melbourne's Flower & Garden Show"]
+  }
 
 }];
 
